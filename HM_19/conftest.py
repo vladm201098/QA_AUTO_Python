1f2c4206df20ec49559b47cb813ba661b084b150
vladm201098 <96441314+vladm201098@users.noreply.github.com>
2022-05-05 15:55:19 +0200

    Update conftest.py

--- HM_19/conftest.py
+++ HM_19/conftest.py
@@ -6,14 +6,14 @@ from school import *
 def students():
     print('\nAdding students to school')
     school = School()
-    anastasiya = Students("Skachek", 1, [9, 10, 9, 10, 9])
-    mirsad = Students("Proshych", 2, [8, 9, 8, 9, 7])
-    yauheni = Students('Vinahradau', 2, [5, 5, 6, 6, 5])
-    aliaksei = Students("Zhendi", 3, [8, 7, 7, 8, 7])
-    school.add_student(anastasiya)
-    school.add_student(mirsad)
-    school.add_student(yauheni)
-    school.add_student(aliaksei)
+    ulyana = Students("Hampe", 1, [9, 10, 9, 10, 9])
+    vladislav = Students("Medvedev", 2, [8, 9, 8, 9, 7])
+    dmitry = Students('Sadovskiy', 2, [5, 5, 6, 6, 5])
+    alesya = Students("Kapashilova", 3, [8, 7, 7, 8, 7])
+    school.add_student(ulyana)
+    school.add_student(vladislav)
+    school.add_student(dmitry)
+    school.add_student(alesya)
     yield school
     del school.students[:]
     print('\nThere is no any student at school')
